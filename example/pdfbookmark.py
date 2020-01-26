275d6e8a511f922314e9fc931fb156fe4e95ab65
Florestan-Eusebius <wang_yff@outlook.com>
2020-01-26 18:31:52 +0800

    bugs fixed, exmp added

---
+++ example/pdfbookmark.py
@@ -0,0 +1,48 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import re
+
+class PdfModify:
+    def __init__(self, inputfile, contents, shiftpage=0):
+        self.input=PdfFileReader(open(inputfile,'rb'))
+        self.output=PdfFileWriter()
+        self.output.cloneDocumentFromReader(self.input)
+        self.shift=shiftpage
+        fl=open(contents,'r',encoding='utf-8')
+        self.bookmarklist=fl.readlines()
+        fl.close()
+        self.rank=[0]
+        self.parent=[]
+        self.outputtitle="bookmarked-"+inputfile
+    def add_one_bookmark(self, rawbookmark):
+        i=0
+        while re.match(r"\s",rawbookmark[i]):
+            i=i+1
+        l=rawbookmark[i:]
+        content=re.sub(r"\s+\d+(\n|$)",'',l)
+        p=re.findall(r"\d+",rawbookmark)
+        page=int(p[-1])+self.shift-1
+        n=-1
+        if i<=self.rank[n]:
+            while i<self.rank[n]:
+                n=n-1
+            self.rank=self.rank[:n]
+            self.parent=self.parent[:n]
+        self.rank.append(i)
+        if self.rank[-1]==0:
+            newparent=self.output.addBookmark(content,page)
+            self.parent.append(newparent)
+        else:
+            newparent=self.output.addBookmark(content,page,self.parent[-1])
+            self.parent.append(newparent)
+    def add_bookmarks(self):
+        for rawbookmark in self.bookmarklist:
+            self.add_one_bookmark(rawbookmark)
+        with open(self.outputtitle,'wb') as fout:
+            self.output.write(fout)
+            
+inputfile=input("Please input the filename (end with \'.pdf\'): ")
+contents=input("Please input the filename of you contents (end with \'.txt\'): ")
+shiftpage=int(input("Please input the shifted page number: "))
+
+filee=PdfModify(inputfile,contents,shiftpage)
+filee.add_bookmarks()
